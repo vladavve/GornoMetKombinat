cfc513a8afc293c82b643d3391d13d78381ab2c2
Vlada <vveibb@gmail.com>
2024-04-05 07:34:50 +0700

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GornoMetKombinat
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ RolAdm.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GornoMetKombinat
+{
+    /// <summary>
+    /// Логика взаимодействия для RolAdm.xaml
+    /// </summary>
+    public partial class RolAdm : Window
+    {
+        public RolAdm()
+        {
+            InitializeComponent();
+        }
+    }
+}
